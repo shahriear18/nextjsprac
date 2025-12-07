157ff4485ec59ce173fe398570e292f6ac2e62be
Shahriear AL Amin <rion7213@gmail.com>
2025-12-07 17:51:58 +0600

    practice..

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 console.log("Hello world");
+
+const http = require("http");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello");
+    res.end();
+  }
+
+  if(req.url === "/about"){
+    res.setHeader("content-type","text/html")
+    res.write("<h1 className = 'text-[red]'>This is an about page...</h1>");
+    res.end();
+  }else{
+    res.end("404")
+  }
+});
+
+let port = 3000;
+server.listen(port, () => {
+  console.log("Server is running");
+});
